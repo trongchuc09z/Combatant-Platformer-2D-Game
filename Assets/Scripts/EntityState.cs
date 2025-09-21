e8688b947bc5dd83ee16c4348952ea02982a4778
TrongChuc <nguyentrongchuc2k4@gmail.com>
2025-09-21 17:18:30 +0700

    first commit

---
+++ Assets/Scripts/EntityState.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public abstract class EntityState
+{
+    protected StateMachine stateMachine;
+    protected string animBoolName;
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    protected float stateTimer;
+    protected bool triggerCalled;
+
+    public EntityState(StateMachine stateMachine, string animBoolName)
+    {
+        this.stateMachine = stateMachine;
+        this.animBoolName = animBoolName;
+    }
+
+
+
+    public virtual void Enter()
+    {
+        anim.SetBool(animBoolName, true);
+        triggerCalled = false;
+    }
+
+    public virtual void Update()
+    {
+        stateTimer -= Time.deltaTime;
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+
+    }
+
+    public virtual void Exit()
+    {
+        anim.SetBool(animBoolName, false);
+    }
+
+    public void CallAnimationTrigger()
+    {
+        triggerCalled = true;
+    }
+
+
+}
